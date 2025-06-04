d3b1e9b8a0479e8a77b88c8c2997235fc5ef33dc
Åsa Johansson <asa.j.1989@gmail.com>
2025-06-04 19:04:04 +0200

    Update origins handling with null check.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,24 +7,28 @@ using WebApi.Repositories;
 using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get< string[]>();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+if(allowedOrigins == null || allowedOrigins.Length == 0)
+{
+    throw new Exception($"Appsettings not loaded correctly. {allowedOrigins}");
+}
 
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins(allowedOrigins!)
+        policy.WithOrigins(allowedOrigins)
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
 });
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
 builder.Services.AddGrpc();
 
 builder.Services.AddDbContext<DataContext>(options =>
